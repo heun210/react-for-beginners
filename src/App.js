b65f10adfbcc8874b793a1acc3c223b69cd3138f
heun <heun210@gmail.com>
2022-04-28 18:04:16 +0900

    Enter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
+
+function Hello() {
+  /* function hiFn() {
+    console.log("Hi :)");
+    return byeFn;
+  }
+  function byeFn() {
+    console.log("bye :(");
+  }
+  useEffect(hiFn, []); */
+
+  useEffect(() => {
+    console.log("hihi :)");
+    return () => console.log("byebye :(");
+  });
+  useEffect(function () {
+    console.log("hi :}");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
-    <dic>
-      <h1>Welcome back React</h1>
-      <Button text={"continue"} />
-    </dic>
+    <div>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
+    </div>
   );
 }
 
